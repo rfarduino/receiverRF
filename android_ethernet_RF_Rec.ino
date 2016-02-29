69c82d5abe5a58933b88c5fe34ee7233e4162d74
lovejoy777 <salovejoy777@gmail.com>
2016-02-29 15:16:06 +0000

    reworked to match new transmitter sketch.

--- android_ethernet_RF_Rec.ino
+++ android_ethernet_RF_Rec.ino
@@ -5,35 +5,29 @@ to be controlled by ethernet/rf base station.
 */
 
 #include <RCSwitch.h>
-
-#define relay1 5 // define pin 5 for relay1
-
+#define relay1 5
 RCSwitch mySwitch = RCSwitch();
 
 void setup() {
   Serial.begin(9600);
   pinMode(relay1, OUTPUT);
-  pinMode(7, OUTPUT); // 5v power for relay
+  pinMode(7, OUTPUT);
   digitalWrite(7, HIGH);
+  digitalWrite(5, LOW);
   mySwitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
 }
 
 void loop() {
   if (mySwitch.available()) {
-    
     int value = mySwitch.getReceivedValue();
-    
     if (value == 0) {
-      Serial.print("No Data");
     } else {
-     // Serial.print("Received ");
-     if (value == 5392) { // if 5392 is received turn relay1 on
-       digitalWrite(relay1,HIGH); // ON
+     if (value == 5394) {
+       digitalWrite(relay1,LOW); // off
      }
-     else if (value == 5393) { // if 5393 is received turn relay1 off
-       digitalWrite(relay1,LOW); // OFF
+     else if (value == 5395) {
+       digitalWrite(relay1,HIGH); // on
      }
-     // Serial.println(value);
     }
     mySwitch.resetAvailable();
   }
